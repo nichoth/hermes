329f917471b9fd76cbc382feb244f76164c39a6e
nick thomas <nichoth@gmail.com>
2022-09-17 13:04:14 -0700

    WIP

--- src/index.tsx
+++ src/index.tsx
@@ -1,144 +1,92 @@
 import { h, render } from 'preact'
 import * as wn from "webnative"
-import { WebnativeProvider } from "./context/webnative"
+wn.setup.debug({ enabled: true })
+// import { WebnativeProvider } from "./context/webnative"
 import { useEffect, useState } from 'preact/hooks'
 import { Permissions } from "webnative/ucan/permissions"
-import { FunctionComponent } from 'preact';
-import { useWebnative } from "./context/webnative"
-// var route = require('route-event')()
 // @ts-ignore
-import Route from 'route-event'
-const route = Route()
-
-console.log('Route', Route)
-
-// const PERMISSIONS = {
-//   app: {
-//     name: "Hermes",
-//     creator: "snail-situation",
-//   },
-//   fs: {
-//     public: [wn.path.directory("Apps", "snail-situaion", "Hermes")],
-//   }
-// }
-
-// interface Props {
-//     permissions?: Permissions
-// }
-
-// const App: FunctionComponent<Props> = function App (props)  {
-//     const arr = useWebnative()
-//     console.log('arr', arr)
-//     // console.log('...props...', props, fs, username)
-//     return (<div>hello from here</div>)
-// }
-
-// const el = document.getElementById('root')
-// if (el) {
-//     render(
-//         <WebnativeProvider permissions={PERMISSIONS}>
-//             <App />
-//         </WebnativeProvider>,
-//         el
-//     )
-// }
-
-
-
-
-// navigation.addEventListener('navigate', navigateEvent => {
-//     // Exit early if this navigation shouldn't be intercepted.
-//     // The properties to look at are discussed later in the article.
-//     if (shouldNotIntercept(navigateEvent)) return
-//     console.log('navigate', navigateEvent)
-  
-//     const url = new URL(navigateEvent.destination.url)
-//     console.log('url', url)
-  
-//     if (url.pathname === '/') {
-//       navigateEvent.intercept({handler: loadIndexPage})
-//     } else if (url.pathname === '/cats/') {
-//       navigateEvent.intercept({handler: loadCatsPage})
-//     }
-// })
-
-
-const state = { route: null }
-route(function onRoute (path:string) {
-    console.log('**on route**', path)
-    state.route = path
-    window.scrollTo(0, 0)
-    console.log('***state', state)
-})
-
+import observ from 'observ'
+// @ts-ignore
+import struct from 'observ-struct'
+import { FunctionComponent } from 'preact';
+// import { useWebnative } from "./context/webnative"
 
 const PERMISSIONS = {
-    app: {
-        name: "Hermes",
-        creator: "snail-situation",
-    },
-    fs: {
-        public: [wn.path.directory("apps", "snail-situation", "hermes")],
-    },
+  app: {
+    name: "Hermes",
+    creator: "snail-situation",
+  },
+  fs: {
+    public: [wn.path.directory("Apps", "snail-situaion", "Hermes")],
+  }
 }
+
+const state = struct({
+    route: observ(location.pathname),
+    init: observ({})
+})
+
+// @ts-ignore
+navigation.addEventListener('navigate', ev => {
+    console.log('navigate', ev)
+    const url = new URL(ev.destination.url)
+    console.log('url', url)
+    console.log('location', location)
+
+    if (url.host !== location.host) return
+
+    ev.intercept({
+        handler () {
+            state.route.set(url.pathname)
+        }
+    })
+})
+
+// @ts-ignore
+state(function onChange (_state) {
+    const { route, init } = _state
+    // console.log('state change', _state)
+    const el = document.getElementById('root')
+    if (!el) return
+    render(<App init={init} route={route} permissions={PERMISSIONS} />, el);
+})
 
 interface Props {
-    permissions?: Permissions
+    route: string,
+    permissions?: Permissions,
+    init?: wn.State
 }
 
-// function login () {
-//     wn.redirectToLobby(state.permissions)
-// }
-
-const App: FunctionComponent<Props> = function App ({ permissions })  {
-    const [state, setState] = useState<wn.State>()
-    const [error, setError] = useState()
-
+const App: FunctionComponent<Props> = function App ({ permissions, route, init })  {
     useEffect(() => {
         async function getState() {
             const result = await wn.initialise({ permissions })
                 .catch((err) => {
-                    setError(err)
+                    if (err) console.log('errrrrrrrrr', err)
                     return undefined
                 })
             
-            setState(result)
+            state.init.set(result)
         }
         getState()
     }, [permissions])
 
-    console.log('state', state)
-    console.log('errrrr', error)
-
-    if (!state) return null
-
-    if (!state?.authenticated) {
+    if (!init?.authenticated) {
         // go to login page
         return (<div>
+            <p>the route is: {route}</p>
             <p>need to auth</p>
             <a href="/fooo">fooo</a>
         </div>)
     }
 
     return (<div class="testing">
-        hello, this is the app
+        <p>the route is: {route}</p>
+        <p>hello, this is the app</p>
     </div>)
 }
 
-async function start () {
-    const res = await wn.initialise({ permissions: PERMISSIONS })
-    console.log('res', res)
-}
-
-start().then()
-
-
-// render(<${Connector} emit=${emit} state=${state}
-//     setRoute=${route.setRoute} client=${client}
-// />, document.getElementById('content'))
-
-
 const el = document.getElementById('root')
 if (el) {
-    render(<App permissions={PERMISSIONS} />, el);
+    render(<App route={''} permissions={PERMISSIONS} />, el)
 }
