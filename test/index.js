0cfcb78b87be01730b62eb610b29886bcd9e2665
nick thomas <nichoth@gmail.com>
2023-02-12 13:21:50 -0800

    WIP

--- test/index.js
+++ test/index.js
@@ -1,9 +1,9 @@
 "use strict";
 exports.__esModule = true;
 // server side tests -- running in a node environment
-var test = require("tape");
+var tape_1 = require("tape");
 var username_js_1 = require("../netlify/functions/username/username.js");
-test('set username', function (t) {
+(0, tape_1["default"])('set username', function (t) {
     // { value, ucan, signature }
     var ev = {
         httpMethod: 'POST',
